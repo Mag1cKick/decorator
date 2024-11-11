d608c0a7a7a9d89df52f24e6042bb8704896adfb
Nazar <novabucha@MacBook-Air-Nova.local>
2024-11-11 20:09:23 +0200

    added test and user

--- demo/src/main/java/flower/store/demo/PaperDecorator.java
+++ demo/src/main/java/flower/store/demo/PaperDecorator.java
@@ -1,15 +1,12 @@
 package flower.store.demo;
 
 public class PaperDecorator extends ItemDecorator{
-
     private final Item item;
-
-    PaperDecorator(Item item){
+    public PaperDecorator(Item item){
         this.item = item;
     }
-
+    @Override
     public double getPrice(){
         return  13 + item.getPrice();
     }
-
 }
